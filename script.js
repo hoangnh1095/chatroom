96175c29af951d931539f87a84d867a4c3e66851
Glitch (hello-webpage) <none>
2020-10-09 10:21:54 +0000

    🏌💴 Checkpoint ./script.js:311760/4562 ./index.html:311760/45

--- script.js
+++ script.js
@@ -2,25 +2,23 @@ const vm = new Vue({
   el: "#app",
   data: {
     message: "🐵 Hello World 🔮",
-    userId: '',
-    userToken: '',
-    roomId: '',
-    roomToken: '',
-    stringeeClient: undefined,
-    joinRoom: false,
+    userId: "",
+    userToken: "",
+    roomId: "",
+    roomToken: "",
+    callClient: undefined,
+    joinRoom: false
   },
   computed: {
     roomUrl: function() {
-      return `https://${location.hostname}?room=${this.roomId}`
+      return `https://${location.hostname}?room=${this.roomId}`;
     }
   },
   async mounted() {
-    api.setRestToken()
-    
-    this.userId = 'hoang dep trai' // window.prompt('Bạn tên gì ahihi?')
-    
-    const urlParams = new URLSearchParams(location.search)
-    const roomId = urlParams.get('room')
+    api.setRestToken();
+
+    const urlParams = new URLSearchParams(location.search);
+    const roomId = urlParams.get("room");
     if (roomId) {
       this.roomId = roomId;
       this.joinRoom = true;
@@ -28,25 +26,36 @@ const vm = new Vue({
   },
   methods: {
     login: async function() {
-      // Lay token tu user_id
-      api.getUserToken(this.userId)
+      this.userId = "hoang_dep_trai"; // window.prompt('Bạn tên gì ahihi?')
+
+      const userToken = await api.getUserToken(this.userId);
+      this.userToken = userToken;
+
+      if (!this.callClient) {
+        const client = new StringeeClient();
+
+        client.on("authen", function(res) {
+          console.log("on authen: ", res);
+        });
+        this.callClient = client;
+      }
+      this.callClient.connect(userToken);
     },
-    createRoom: async function () {
-      const room = await api.createRoom()
-      const {roomId} = room
-      const roomToken = await api.getRoomToken(roomId) 
-      
-      this.roomId = roomId
-      this.roomToken = roomToken
-      console.log({roomId, roomToken})
-      
+    createRoom: async function() {
+      const room = await api.createRoom();
+      const { roomId } = room;
+      const roomToken = await api.getRoomToken(roomId);
+
+      this.roomId = roomId;
+      this.roomToken = roomToken;
+      console.log({ roomId, roomToken });
+
       // Create xong join room lun
+      await this.login();
     },
-    joinRoom: async function () {
+    joinRoom: async function() {
       // Lay room id
-      
       // Lay room token
-      
       // join room lam tro
     }
   }
