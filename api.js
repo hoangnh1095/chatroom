ad9c24eb764e4b4f7fe4e68ce32a9649be1a87e0
Glitch (hello-webpage) <none>
2020-10-20 10:23:36 +0000

    🚪🍔 Checkpoint ./api.js:311760/250

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
-const PROJECT_ID = "SK5k11B6oUJvZyFxEHY1dexbjN7AR6JKFt";
-const PROJECT_SECRET = "YlJrUUh6bDJYcFZ2bVRiMTJQbjZHWU1lUVduODZZ";
+const PROJECT_ID = "SKIuNgIgQclWM0GFSPCJO7eervWS7hKkQg";
+const PROJECT_SECRET = "Tk1NaUZGVElWZ3VpNFZVQlZVVTZseWFsWTlHTndyMw==";
 const BASE_URL = "https://api.stringee.com/v1/room2";
 
 class API {
