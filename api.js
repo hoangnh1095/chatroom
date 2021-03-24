abb60255f42280994cf93e7b0eb39f3a5de51ea7
Glitch (hello-webpage) <none>
2021-03-24 17:56:23 +0000

    🍅🚵 Checkpoint ./api.js:30137197/68

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-const PROJECT_ID = "SKIuNgIgQclWM0GFSPCJO7eervWS7hKkQg";
+const PROJECT_ID = "SKTGeANqdbrBPsJqevZz3YPdJQs32W7ajR";
 const PROJECT_SECRET = "Tk1NaUZGVElWZ3VpNFZVQlZVVTZseWFsWTlHTndyMw==";
 const BASE_URL = "https://api.stringee.com/v1/room2";
 
